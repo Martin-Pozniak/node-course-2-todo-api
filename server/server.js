8913e3532c216629d6eabbedd2e36fb46f3b9efe
Martin-PC\Martin <cap.pozniak@gmail.com>
2017-12-25 15:19:18 -0600

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -41,6 +41,16 @@ app.post('/todos', (req,res) => {
     });
 });
 
+app.get('/todos',(req,res) => {
+    Todo.find().then( (todos) => {
+        res.send({
+            todos
+        });
+    }, (e) => {
+        res.statusCode(400).send(e);
+    });
+});
+
 
 /*****************************************************************************************************
 *start the web server on port 3000 localhost
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,28 +1,39 @@
-const expect = require('expect');
-const request = require('supertest');
+const expect = require('expect'); //expect is a module used to create assertions. assertions are test cases that help us simplify the process of writing complicated if else blocks to test code
+const request = require('supertest'); //request by supertest is used to help us easy made http requests so that we can test the code.
 
-const {app} = require('./../server.js');
-const {Todo} = require('./../models/Todos.js');
+const {app} = require('./../server.js'); //we require app from the server.js file
+const {Todo} = require('./../models/Todos.js'); //we require the Todo object from the Todos.js file | it cointains a mongoose model that is used to add things to the database.
 
-beforeEach( (done) => {
+/*****************************************************************************************************
+*Before each is code that will execute asynchronoously before allowing the code to continue
+*In this case, we are clearing the todos from the db before running the test case to make sure we can properly test
+*the number of items in the collection.
+*****************************************************************************************************/
+beforeEach( (done) => { 
     Todo.remove({}).then( () => {
         done();
     });
 });
 
+/*****************************************************************************************************
+*Writes a describe block that contains our mocha and express test cases.
+*****************************************************************************************************/
 describe("POST /todos", () => {
 
+    /*******************************************
+    *This test case checks that the entry has been added to the DB
+    *******************************************/
     it("should create a new todo", (done) => {
         var text = 'Test todo text';
 
         request(app)
-            .post('/todos')
-            .send({text})
-            .expect(200)
-            .expect( (res) => {
+            .post('/todos') //specifies the request type
+            .send({text}) //sends the request body
+            .expect(200) //asserts that we want a 200 status code
+            .expect( (res) => { //asserts that we want the response body to be equal to the post body we made
                 expect(res.body.text).toBe(text);
             })
-            .end( (err,res) => {
+            .end( (err,res) => { //before we end, we check that the response was actually added to the DB
                 if (err) return done(err);
                 
                 Todo.find().then((todos) => {
@@ -34,6 +45,9 @@ describe("POST /todos", () => {
        
     });
 
+    /*******************************************
+    *This test case checks that the entry has NOT been added to the DB
+    *******************************************/
     it('should not create todo with invalid body data', (done) => {
 
         request(app)
